a5a71d091bc9791f35d34171c2914d0fc1780d42
Yuxing <qingfeng@udel.edu>
2021-10-30 10:46:23 +0800

    Update App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,1 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import App from "./App";
 
-test("AgGridReact", () => {
-    render(<App />);
-    const linkElement = screen.getByText(/AgGridReact/i);
-    expect(linkElement).toBeInTheDocument();
-});
