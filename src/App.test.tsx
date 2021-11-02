48cdc882e444a502093242704d5a697d4e758103
Lufeng <chenlf@udel.edu>
2021-11-02 22:29:45 +0800

    fix test

---
+++ src/App.test.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+test("renders learn react link", () => {
+    // Arrange: prepare the environment, 
+    //         render the component.
+    render(<App />);
+    
+    // Act: try to find the expected link.
+    const linkElement = screen.getByText(/learn react/i);
+    
+    // Assert: check that required link 
+    //        is indeed in the document.
+    expect(linkElement).toBeInTheDocument();
+});
