51d492966c7fd0f95d4ac7c99f840b41829dfdef
Yuxing Wei <qingfeng@udel.edu>
2021-11-11 07:19:54 -0500

    add new semester and add new course in one semeste

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React, {useState} from "react";
+import React, {useState,useEffect} from "react";
 import {AgGridReact} from "ag-grid-react";
+
 import "ag-grid-community/dist/styles/ag-grid.css";
 import "ag-grid-community/dist/styles/ag-theme-alpine.css";
-
 
 
 
@@ -33,31 +33,116 @@ const App = () => {
     const [show,setshow] = useState(true);
 
     const [semesterLevel,setSemesterLever] = useState(["Semester1","Semester2","Semester3"]);
+    
+    const [newData,setNewData] = useState([
+        {
+            "rowData":[
+                {id:0,Course: "EGGG101", Credit: "2",Name:"engineer101",Plan:"Take Care"},
+                {id:0,Course: "CISC108", Credit: "3",Name:"Computer Science108",Plan:"Take Care"},
+                {id:0,Course: "MATH241", Credit: "4",Name:"Mathematic241",Plan:"Take Care"},
+                {id:0,Course: "ENGL101", Credit: "3",Name:"engineer101",Plan:"Take Care"},
+                {id:0,Course: "BRE", Credit: "3",Name:"Breath",Plan:"Take Care"},
+            ], 
+        },
+        {
+            "rowData":[
+                {id:2,Course: "EGGG103", Credit: "2",Name:"engineer103",Plan:"Take Care"},
+                {id:2,Course: "CISC110", Credit: "3",Name:"Computer Science110",Plan:"Take Care"},
+                {id:2,Course: "MATH243", Credit: "4",Name:"Mathematic243",Plan:"Take Care"},
+                {id:2,Course: "ENGL103", Credit: "3",Name:"engineer103",Plan:"Take Care"},
+                {id:2,Course: "BRE2", Credit: "3",Name:"Breath3",Plan:"Take Care"},
+            ]
+        }
+    ]);
+    
+    
+    const [rowDataIndex,setRowDataIndex] = useState(0);
+    const [rowIndex,setRowIndex] = useState(0);
 
     const actionButton = (params:any)=>{
-        const semesterid = params.data["id"];
-        if (semesterid == 0){
-            setRowData((prevData)=>{
-                return prevData.filter(param => param != params.data );
+        const semesterid = (params.data.id);
+        console.log(params.data);
+        newData.forEach((value,index)=>{
+            console.log(value);
+            value.rowData.forEach((value1,index1)=>{
+                if (value1 == params.data){
+                    const newIndex = JSON.parse(JSON.stringify(index));
+                    const newIndex1 = JSON.parse(JSON.stringify(index1));
+                    setRowDataIndex(newIndex);
+                    setRowIndex(newIndex1);
+                    
+                }
             });
-        }
-        if (semesterid == 1){
-            setRowData1((prevData)=>{
-                return prevData.filter(param => param != params.data );
-            });
-        }
-        if (semesterid == 2){
-            setRowData2((prevData)=>{
-                return prevData.filter(param => param != params.data );
-            });
-        }
+        });
+        const newData2 = JSON.parse(JSON.stringify(newData));
+        const index = newData2;
+        newData2[rowDataIndex]["rowData"].splice(rowIndex,1);
+        setNewData(newData2);
+        console.log(rowDataIndex);
+        console.log(rowIndex);
+        // if (semesterid == 0){
+
+        //     const func = (element: unknown) =>{
+        //         // console.log(typeof element);
+        //         // console.log(element != params.data);
+        //         return element != params.data;
+        //     };
+           
+        //     const list = [...newData[0].rowData];
+        //     const newlist = list.filter(func);
+        //     console.log(newlist);
+        //     // const index = newData[0].rowData.indexOf(params.data);
+        //     // console.log(index);
+            
+        //     // newData.splice(0,1);
+        //     // const newData1 = newData;
+        //     // setNewData(newData);
+            
+        //     newData.forEach((value,index)=>{
+        //         console.log(value);
+        //         value.rowData.forEach((value1,index1)=>{
+        //             if (value1 == params.data){
+        //                 setRowDataIndex(index);
+        //                 setRowIndex(index1);
+        //             }
+        //         });
+        //     });
+        //     console.log(rowDataIndex);
+        //     const newData2 = JSON.parse(JSON.stringify(newData));
+        //     const index = newData2;
+        //     newData2[0]["rowData"].splice(0,1);
+        //     setNewData(newData2);
+            
+            
+            
+        
+        // }
+        
+    };
+    const addArow = (index: number) => {
+        
+        const newCourse = {id:index,Course: "", Credit: "",Name:"",Plan:""};
+        console.log(newCourse);
+        const tmpNewData = JSON.parse(JSON.stringify(newData));
+        tmpNewData[index].rowData.push(newCourse);
+        console.log(tmpNewData[index]);
+        setNewData(tmpNewData);
+        
+    };
+
+    const addAsemester = () =>{
+        const index = newData.length;
+        const tmpNewData = JSON.parse(JSON.stringify(newData));
+        const newSemester = {rowData:[]};
+        tmpNewData.push(newSemester);
+        setNewData(tmpNewData);
     };
     const columns = [
         {
-            headerName:"course",field:"Course",sortable:true,
+            headerName:"course",field:"Course",sortable:true,editable:true,
         },
         {
-            headerName:"credit",field:"Credit",sortable:true,
+            headerName:"credit",field:"Credit",sortable:true,editable:true,
         },
         {
             headerName:"name",field:"Name",sortable:true,editable:true,
@@ -73,35 +158,33 @@ const App = () => {
             </div>,
             
         },
+        
     ];
+    
 
     return (
         
-        <div className="ag-theme-alpine" style={{height: 400, width: 1000}}>
-            <button onClick={()=>setshow(false)}>Clear All Semester</button> <button onClick={()=>setshow(true)}>Show All Semester</button>
+        <div>
             {
+                newData.map((value,index) => (
+                    <div key = {index}>
+                        
+                        <div className="ag-theme-alpine" style={{height: 400, width: 1000}}>
+                            
+                            <AgGridReact rowData={value.rowData} columnDefs={columns}/>
+                            
+                        </div>
+                        
+                        <button onClick = {()=>addArow(index)}>addRow</button>
+                    </div>
+                ))
                 
-                show?
-                    
-                    <AgGridReact
-                        rowData={rowData} columnDefs={columns} />
-                    
-                    :null
-
             }
-            {
-                show?
-                   
-                    <AgGridReact
-                        rowData={rowData1} columnDefs={columns}/>
-                    :null
-            }
-            {
-                show?
-                    <AgGridReact
-                        rowData={rowData2} columnDefs={columns}/>
-                    :null
-            }   
+            <button onClick={()=>setshow(false)}>Clear All Semester</button> <button onClick={()=>setshow(true)}>Show All Semester</button>
+            <button onClick = {() => addAsemester()}>add newSemester</button>
+            
+           
+           
         </div>
     );
 };
