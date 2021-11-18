6ef90c47e514c6b5221e5687c23ee9419e252987
Lufeng <chenlf@udel.edu>
2021-11-18 22:49:28 +0800

    fix any

--- src/App.tsx
+++ src/App.tsx
@@ -122,6 +122,12 @@ const App = () => {
         setNewData(JsonData);
         console.log(newData);
     };
+
+    const clearAllCourse = (index:number)=>{
+        const tmpNewData = JSON.parse(JSON.stringify(newData));
+        tmpNewData[index].rowData = [];
+        setNewData(tmpNewData);
+    };
     
     const [show1, setShow1] = useState(true);
 
@@ -142,7 +148,7 @@ const App = () => {
                             </div>
                             
                             <button onClick = {()=>addArow(index)} style={{marginLeft:350}}>AddCourse</button>
-                            
+                            <button onClick={()=>clearAllCourse(index)}>clear All Course</button>
                         </div>
     
                         
